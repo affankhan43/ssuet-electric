24149f3766138a4418f72c1949023c1e4e3d20a1
unknown <affankhan627@gmail.com>
2019-10-28 00:34:44 +0500

    stat

--- app/Http/Controllers/Api/dataController.php
+++ app/Http/Controllers/Api/dataController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
+use App\Stat;
 use Tymon\JWTAuth\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Validator, DB, Hash;
@@ -15,9 +16,27 @@ class dataController extends Controller
     //
     private $user;
 	private $jwtauth;
+	private $stats;
 	
-	public function __construct(User $user, JWTAuth $jwtauth){
+	public function __construct(User $user, JWTAuth $jwtauth, Stat $stats){
 		$this->user = $user;
 		$this->jwtauth = $jwtauth;
+		$this->stats = $stats;
+	}
+
+	public function Stats(Request $request){
+		$user = $this->jwtauth->parseToken()->authenticate();
+		if($user){
+			$stats = $this->stats->first();
+			if($stats){
+				return response()->json(['success'=>true,'stats'=>$stats]);
+			}
+			else{
+				return response()->json(['success'=>false,'message'=>'Stats Not Available! Connection Not Build']);
+			}
+		}
+		else{
+			return response()->json(['success'=>false,'message'=>"invalid_user_make_logout"]);
+		}
 	}
 }
