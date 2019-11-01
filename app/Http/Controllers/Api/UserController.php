fcd86f52bf903e63fbb0c61455509f47ee66b5f7
unknown <affankhan627@gmail.com>
2019-11-02 04:04:41 +0500

    jwt

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,21 +5,53 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
+use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Http\Controllers\Controller;
+use Validator, DB, Hash;
+use Illuminate\Support\Facades\Password;
 
 class UserController extends Controller
 {
-    //
-    private $user;
+	//
+	private $user;
+	private $jwtauth;
+	
+	public function __construct(User $user, JWTAuth $jwtauth){
+		$this->user = $user;
+		$this->jwtauth = $jwtauth;
+	}
 
-    function __construct(User $user){
-    	$this->user = $user;
-    }
+	public function register(Request $request){
+		$credentials = $request->only('name', 'email', 'password');
+		$rules = [
+			'name' => 'required|max:255',
+			'email' => 'required|email|max:255|unique:users',
+			'password'=>'required|min:8'
+		];
+		$validator = Validator::make($credentials, $rules);
+		if($validator->fails()) {
+			return response()->json(['success'=> false, 'error'=> $validator->messages()]);
+		}
+		$user = $this->user->create(['name'=>$request->name,'email'=>$request->email,'password'=>Hash::make($request->password)]);
+		return response()->json(['success'=> true, 'message'=> 'Thanks for signing up!']);
+	}
 
-    public function login(Request $request){
-    	if($request->isMethod('post')){
+	public function login(LoginRequest $request){
+		// get user credentials: email, password
+		$credentials = $request->only('email', 'password');
+		$token = null;
+		try{
+			$token = $this->jwtauth->attempt($credentials);
+			if(!$token){
+				return response()->json(['invalid_email_or_password'], 422);
+			}
+		}
+		catch (JWTAuthException $e) {
+			return response()->json(['failed_to_create_token'], 500);
+		}
+		$user = $this->jwtauth->setToken($token)->toUser();
+		return response()->json(['id'=>$user->id,'email'=>$user->email,'name'=>$user->name,'token'=>$token]);
+	}
 
-    	}else{
-    		return response()->json(['success'=>false,'message'=>'Invalid Request']);
-    	}
-    }
 }
