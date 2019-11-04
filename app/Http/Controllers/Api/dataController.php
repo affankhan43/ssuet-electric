817cfd938b345736e5e062dc5d1849508bc7062b
unknown <affankhan627@gmail.com>
2019-11-04 20:31:48 +0500

    fetch_wallet

--- app/Http/Controllers/Api/dataController.php
+++ app/Http/Controllers/Api/dataController.php
@@ -61,6 +61,7 @@ class dataController extends Controller
 	public function fetchWallet(Request $request){
 		$user = $this->jwtauth->parseToken()->authenticate();
 		if($user){
+			print_r($user)
 			$wallet_data = $this->wallets->where('user_id',$user->id)->first();
 			if($wallet_data){
 				return response()->json(['success'=>true,'wallet'=>$wallet_data]);
