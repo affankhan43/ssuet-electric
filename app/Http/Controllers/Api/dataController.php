fdf0e9043332df9f4c75ab9774a15db577019cf3
unknown <affankhan627@gmail.com>
2019-11-05 20:55:42 +0500

    fetchWallet

--- app/Http/Controllers/Api/dataController.php
+++ app/Http/Controllers/Api/dataController.php
@@ -65,7 +65,7 @@ class dataController extends Controller
 			echo $user->id;
 			echo $user['id'];
 			echo json_encode($user);
-			$wallet_data = $this->wallets->where(['user_id'=>2])->first();
+			$wallet_data = $this->wallets->where(['user_id'=>2])->get();
 			if($wallet_data){
 				return response()->json(['success'=>true,'wallet'=>$wallet_data]);
 			}
