add017880411d09a95e50c800050bb47ad018a74
unknown <affankhan627@gmail.com>
2019-11-06 15:11:50 +0500

    rate_table

---
+++ database/migrations/2019_11_06_100433_create_rate_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rate', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('rate');
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+        });
+        Schema::table('rate', function($table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rate');
+    }
+}
